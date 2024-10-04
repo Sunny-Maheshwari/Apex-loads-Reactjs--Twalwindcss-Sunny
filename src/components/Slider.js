59396da506c46bd1d72fdf61ecf9c37180409965
Sunny-Maheshwari <maheshwarisunny17@gmail.com>
2024-10-04 18:23:24 +0500

    Cards issue fixed

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -36,42 +36,33 @@ const Slider = () => {
   return (
     <div className="h-screen bg-black flex items-center justify-center p-20">
       <div className="container mx-auto py-12 space-y-20">
-        <div>
+        <div className="flex flex-row justify-between items-center">
           <h3 className="text-6xl font-bold text-white text-left">
             Services We <span class="text-red-600">Offer</span>
           </h3>
+          <div className="hidden sm:flex">
+            <button className="hover:bg-red-400 cursor-pointer bg-[#D91E27] text-white rounded-full btn-sm w-[100px]">
+              Know more
+            </button>
+          </div>
         </div>
         <div className="carousel w-full flex overflow-x-auto space-x-6 snap-x snap-mandatory">
           {creators.map((creator, index) => (
             <div className="card bg-base-100 w-96 shadow-xl">
-            <figure>
-              <img
-                src={creator.img}
-                alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{creator.name}</h2>
-              <p>{creator.description}</p>
-              <div className="card-actions justify-start items-center">
-                <button className="rounded-full bg-[#D91E27] text-white w-[100px] text-center">Know more</button>
+              <figure>
+                <img src={creator.img} alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{creator.name}</h2>
+                <p>{creator.description}</p>
+                <div className="hidden sm:flex">
+                  <button className="hover:bg-red-400 cursor-pointer bg-[#D91E27] text-white rounded-full btn-sm w-[100px]">
+                    Know more
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
           ))}
-        </div>
-        <div className="flex items-center justify-center flex-row space-x-5 mt-10">
-          <a
-            href="#contact"
-            className="btn w-48 rounded-full border-none font-bold bg-[#343434] text-white hover:bg-white hover:text-black focus:outline-none"
-          >
-            View All →
-          </a>
-          <a
-            href="#contact"
-            className="btn w-48  hover:bg-black font-bold border border-[#343434] hover:text-gray-400 rounded-full bg-black text-white focus:outline-none"
-          >
-            Skip Section
-          </a>
         </div>
       </div>
     </div>
